c519ac7473ccfe6ee1678a9efde56c730647f300
Willy Kristianto <willykristianto89@gmail.com>
2025-03-21 23:47:35 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# kuy-chat
+# kuy-chat server
+
+#### Requirement
+- node v22 or higher
+- npm version 10 or higher
+
+#### Usage
+- npm install
+- node index.js
+- open http://localhost:3000
---
+++ index.js
@@ -0,0 +1,33 @@
+// Install dependencies: npm install express socket.io cors
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
